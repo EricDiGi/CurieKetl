d9b37db4313301430491c85b1b43bc90b74bb37b
EricDiGi <eric.digioacchino01@gmail.com>
2023-08-07 16:27:42 -0400

    setup description added

--- src/setup.py
+++ src/setup.py
@@ -11,7 +11,7 @@ setup(name='curie',
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3"
         ],
-        long_description=open('README.md').read(),
+        long_description=open('../README.md').read(),
         long_description_content_type='text/markdown',
       packages=find_packages(),
       entry_points={
