d373e242d84f73a7693e3a4e2fa4120545c7aa4a
EricDiGi <eric.digioacchino01@gmail.com>
2023-08-07 15:47:29 -0400

    attempt at initial release

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,15 @@
 from setuptools import setup, find_packages
 
 setup(name='curie',
-      version='0.1',
+      version='0.0.1',
+      author='Eric DiGioacchino',
+      url='https://github.com/EricDiGi/CurieKetl',
+      keywords=['etl', 'data', 'data engineering', 'data science', 'mlops', 'devops'],
+      classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3"
+        ],
       packages=find_packages(),
       entry_points={
           'console_scripts': [
