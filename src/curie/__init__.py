f3cc38782d21114ddea44869754e47a28d078046
EricDiGi <eric.digioacchino01@gmail.com>
2023-10-03 12:29:28 -0400

    print testing

--- src/curie/__init__.py
+++ src/curie/__init__.py
@@ -170,8 +170,8 @@ class ProjectManager:
         """
         if secretsmanager is not None:
             b3s = Secrets(secretsmanager)
-            print(b3s.secret)
-            return dict(b3s.secret)
+            print(type(b3s.secret))
+            return b3s.secret
     
     def build_connections(self, path:str = None):
         """
